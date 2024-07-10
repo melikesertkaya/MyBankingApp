94c7070adedf79bef51a4129156ce9d5a42b102e
MELIKE SERTKAYA <MELIKE.SERTKAYA@lcwaikiki.com>
2024-07-10 17:05:25 +0300

    Add project files.

---
+++ src/MyBankingApp/BankAccount.cs
@@ -0,0 +1,27 @@
+﻿namespace MyBankingApp
+{
+    public class BankAccount
+    {
+        private decimal _balance;
+
+        public BankAccount()
+        {
+            _balance = 0;
+        }
+
+        public void Deposit(decimal amount)
+        {
+            if (amount <= 0)
+            {
+                throw new ArgumentException("Deposit amount must be positive.");
+            }
+
+            _balance += amount;
+        }
+
+        public decimal GetBalance()
+        {
+            return _balance;
+        }
+    }
+}
---
+++ tests/MyBankingApp.Tests/BankAccountTests.cs
@@ -0,0 +1,45 @@
+﻿using Xunit;
+
+namespace MyBankingApp.Tests
+{
+    public class BankAccountTests
+    {
+        [Fact]
+        public void Deposit_WithPositiveAmount_UpdatesBalance()
+        {
+            // Arrange
+            var account = new BankAccount();
+            var depositAmount = 100m;
+
+            // Act
+            account.Deposit(depositAmount);
+
+            // Assert
+            Assert.Equal(depositAmount, account.GetBalance());
+        }
+
+        [Fact]
+        public void Deposit_WithNegativeAmount_ThrowsArgumentException()
+        {
+            // Arrange
+            var account = new BankAccount();
+            var depositAmount = -50m;
+
+            // Act & Assert
+            Assert.Throws<ArgumentException>(() => account.Deposit(depositAmount));
+        }
+
+        [Fact]
+        public void GetBalance_Initially_ReturnsZero()
+        {
+            // Arrange
+            var account = new BankAccount();
+
+            // Act
+            var balance = account.GetBalance();
+
+            // Assert
+            Assert.Equal(0m, balance);
+        }
+    }
+}
